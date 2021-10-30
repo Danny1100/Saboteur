d09d1baa0f8a67d43c44e069e81a2dbe62409550
Danny Cai <dannycai1100@gmail.com>
2021-10-30 23:33:25 +1100

    final game with deployed links v1

--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
         // origin: "http://localhost:3000"
-        origin: "https://61714298e70df8c1f125e3c7--ecstatic-boyd-abc2f3.netlify.app"
+        origin: "https://ecstatic-boyd-abc2f3.netlify.app"
     }
 });
 
